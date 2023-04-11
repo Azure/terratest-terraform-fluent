1fa12d0d4cc6464edd789a397c39ba16afc46771
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-04-11 20:20:05 +0100

    test: more applydestroy fun

--- setuptest/applydestroy_test.go
+++ setuptest/applydestroy_test.go
@@ -35,10 +35,10 @@ func TestApplyIdempotentRetryFail(t *testing.T) {
 	}
 	test, err := Dirs("testdata/applyidempotentretryfail", "").WithVars(nil).InitPlanShow(t)
 	require.NoError(t, err)
-	defer test.Destroy(t) //nolint:errcheck
+	defer test.Cleanup()
 	tb := time.Now()
 	err = test.ApplyIdempotentRetry(t, rty).AsError()
-	assert.Truef(t, time.Since(tb) >= 20*time.Second, "retry should have waited at least 20 second")
+	assert.Truef(t, time.Since(tb) >= 10*time.Second, "retry should have waited at least 10 second")
 	assert.ErrorContains(t, err, "terraform configuration not idempotent")
 }
 
@@ -47,7 +47,7 @@ func TestApplyFail(t *testing.T) {
 
 	test, err := Dirs("testdata/applyfail", "").WithVars(nil).InitPlanShow(t)
 	require.NoError(t, err)
-	defer test.Destroy(t) //nolint:errcheck
+	defer test.Cleanup()
 	err = test.Apply(t).AsError()
 	assert.ErrorContains(t, err, "test error")
 }
@@ -57,7 +57,7 @@ func TestApplyIdempotentApplyFail(t *testing.T) {
 
 	test, err := Dirs("testdata/applyfail", "").WithVars(nil).InitPlanShow(t)
 	require.NoError(t, err)
-	defer test.Destroy(t) //nolint:errcheck
+	defer test.Cleanup()
 	err = test.ApplyIdempotent(t).AsError()
 	assert.ErrorContains(t, err, "test error")
 }
@@ -71,7 +71,8 @@ func TestApplyIdempotentRetryApplyFail(t *testing.T) {
 	}
 	test, err := Dirs("testdata/applyfail", "").WithVars(nil).InitPlanShow(t)
 	require.NoError(t, err)
-	defer test.Destroy(t) //nolint:errcheck
+
+	defer test.Cleanup()
 	err = test.ApplyIdempotentRetry(t, rty).AsError()
 	assert.ErrorContains(t, err, "test error")
 }
@@ -84,7 +85,56 @@ func TestDestroyRetry(t *testing.T) {
 		Wait: time.Second * 10,
 	}
 	test, err := Dirs("testdata/depth1", "").WithVars(nil).InitPlanShow(t)
+	defer test.Cleanup()
 	require.NoError(t, err)
 	test.ApplyIdempotent(t).ErrorIsNil(t)
 	test.DestroyRetry(t, rty).ErrorIsNil(t)
+}
+
+func TestDestroy(t *testing.T) {
+	t.Parallel()
+
+	test, err := Dirs("testdata/depth1", "").WithVars(nil).InitPlanShow(t)
+	defer test.Cleanup()
+	require.NoError(t, err)
+	test.ApplyIdempotent(t).ErrorIsNil(t)
+	test.Destroy(t).ErrorIsNil(t)
+}
+
+func TestDestroyFail(t *testing.T) {
+	t.Parallel()
+
+	test, err := Dirs("testdata/destroyfailretryok", "").WithVars(nil).InitPlanShow(t)
+	defer test.Cleanup()
+	require.NoError(t, err)
+	test.Apply(t).ErrorIsNil(t)
+	err = test.Destroy(t).AsError()
+	assert.ErrorContains(t, err, "error while running command: exit status 1")
+}
+
+func TestDestroyRetryOnceFail(t *testing.T) {
+	rty := Retry{
+		Max:  1,
+		Wait: time.Second * 10,
+	}
+	test, err := Dirs("testdata/destroyfailretryok", "").WithVars(nil).InitPlanShow(t)
+	defer test.Cleanup()
+	require.NoError(t, err)
+	test.Apply(t).ErrorIsNil(t)
+	err = test.DestroyRetry(t, rty).AsError()
+	assert.ErrorContains(t, err, "error while running command: exit status 1")
+}
+
+func TestDestroyRetryOnce(t *testing.T) {
+	rty := Retry{
+		Max:  2,
+		Wait: time.Second * 10,
+	}
+	test, err := Dirs("testdata/destroyfailretryok", "").WithVars(nil).InitPlanShow(t)
+	defer test.Cleanup()
+	require.NoError(t, err)
+	test.Apply(t).ErrorIsNil(t)
+	tb := time.Now()
+	test.DestroyRetry(t, rty).ErrorIsNil(t)
+	assert.Truef(t, time.Since(tb) >= 10*time.Second, "retry should have waited at least 10 second")
 }
