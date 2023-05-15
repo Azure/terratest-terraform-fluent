a4ef1c14c95c2237e10cc5d9ac835f332e77c4f2
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-05-15 12:19:04 +0100

    test: refactor

--- check/query_test.go
+++ check/query_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/Azure/terratest-terraform-fluent/setuptest"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	tfjson "github.com/hashicorp/terraform-json"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -51,47 +53,60 @@ func TestQueryNestedMap(t *testing.T) {
 func TestQueryNil(t *testing.T) {
 	t.Parallel()
 
-	tftest, err := setuptest.Dirs(queryTestData, "").WithVars(nil).InitPlanShow(t)
-	require.NoError(t, err)
-	defer tftest.Cleanup()
-	assert.NoError(
-		t,
-		InPlan(tftest.Plan).That("terraform_data.invalid_json").Key("input").Query(".").HasValue(nil).AsError(),
-	)
+	twkq := mockThatTypeWithKeyQuery(nil, ".")
+	err := twkq.HasValue(nil).AsError()
+	assert.NoError(t, err)
 }
 
 func TestQueryInvalidArgs(t *testing.T) {
 	t.Parallel()
 
-	tftest, err := setuptest.Dirs(queryTestData, "").WithVars(nil).InitPlanShow(t)
-	require.NoError(t, err)
-	defer tftest.Cleanup()
+	twkq := mockThatTypeWithKeyQuery(nil, ".")
 
-	InPlan(tftest.Plan).That("terraform_data.invalid_json").Key("input").Query(".").HasValue(func() {}).ErrorContains(t, "invalid operation")
+	err := twkq.HasValue(func() {}).AsError()
+	assert.ErrorContains(t, err, "invalid operation")
 }
 
 func TestQueryNotEqual(t *testing.T) {
 	t.Parallel()
 
-	tftest, err := setuptest.Dirs(queryTestData, "").WithVars(nil).InitPlanShow(t)
-	require.NoError(t, err)
-	defer tftest.Cleanup()
-	assert.ErrorContains(
-		t,
-		InPlan(tftest.Plan).That("terraform_data.invalid_json").Key("input").Query(".").HasValue(123).AsError(),
-		"query result <nil>, for key input not equal to assertion 123",
-	)
+	twkq := mockThatTypeWithKeyQuery("{}", "test_key")
+	err := twkq.HasValue(123).AsError()
+	assert.ErrorContains(t, err, "query result <nil>, for key test_key not equal to assertion 123")
 }
 
 func TestQueryNotExists(t *testing.T) {
 	t.Parallel()
 
-	tftest, err := setuptest.Dirs(queryTestData, "").WithVars(nil).InitPlanShow(t)
-	require.NoError(t, err)
-	defer tftest.Cleanup()
-	assert.ErrorContains(
-		t,
-		InPlan(tftest.Plan).That("terraform_data.invalid_json").Key("not_exists").Query(".").HasValue(123).AsError(),
-		"key not_exists not found in resource",
-	)
+	twkq := mockThatTypeWithKeyQuery(nil, "not_exists")
+	twkq.Key = "not_exists"
+	err := twkq.HasValue(nil).AsError()
+	assert.ErrorContains(t, err, "key not_exists not found in resource")
+}
+
+func TestQueryEscapedJson(t *testing.T) {
+	t.Parallel()
+
+	// Create a mock ThatTypeWithKeyQuery object
+	val := "{\"properties\":{\"testProperty\":{\"testArray\":[\"testArrayMember\"]},\"testProperty2\":{\"TestArray2\":[]}}}"
+	twkq := mockThatTypeWithKeyQuery(val, "properties.testProperty.testArray.0")
+	err := twkq.HasValue("testArrayMember")
+	assert.NoError(t, err.AsError())
+}
+
+func mockThatTypeWithKeyQuery(val any, query string) ThatTypeWithKeyQuery {
+	return ThatTypeWithKeyQuery{
+		Plan: &terraform.PlanStruct{
+			ResourcePlannedValuesMap: map[string]*tfjson.StateResource{
+				"test_resource": {
+					AttributeValues: map[string]interface{}{
+						"test_key": val,
+					},
+				},
+			},
+		},
+		ResourceName: "test_resource",
+		Key:          "test_key",
+		Query:        query,
+	}
 }
