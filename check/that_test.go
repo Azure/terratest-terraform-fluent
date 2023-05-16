3b426e8568d08b3c42517391a5a413735d8f194d
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-05-16 14:45:13 +0100

    test: coverage+

--- check/that_test.go
+++ check/that_test.go
@@ -42,12 +42,39 @@ func TestResourceDoesNotExistFail(t *testing.T) {
 	assert.ErrorContains(t, err, "test_resource: resource found in plan")
 }
 
+func TestKey(t *testing.T) {
+	t.Parallel()
+
+	tt := mockThatType()
+
+	t.Run("Success", func(t *testing.T) {
+		o := tt.Key("key")
+		assert.Equal(t, "value", o.Actual)
+		assert.True(t, o.Exist)
+	})
+
+	t.Run("ResourceNotFound", func(t *testing.T) {
+		tt.ResourceName = "not_exists"
+		o := tt.Key("key")
+		assert.Nil(t, o.Actual)
+		assert.False(t, o.Exist)
+	})
+
+	t.Run("KeyNotFound", func(t *testing.T) {
+		o := tt.Key("not_exists")
+		assert.Nil(t, o.Actual)
+		assert.False(t, o.Exist)
+	})
+}
+
 func mockThatType() ThatType {
 	return ThatType{
 		Plan: &terraform.PlanStruct{
 			ResourcePlannedValuesMap: map[string]*tfjson.StateResource{
 				"test_resource": {
-					AttributeValues: map[string]interface{}{},
+					AttributeValues: map[string]any{
+						"key": "value",
+					},
 				},
 			},
 		},
