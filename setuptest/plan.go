c1ce5f988ac977efbbe13d8f63bb028ffb4a033d
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-05-22 15:24:35 +0100

    fix: remove unused Plan func

--- setuptest/plan.go
+++ setuptest/plan.go
@@ -1,24 +1,10 @@
 package setuptest
 
-import (
-	"github.com/Azure/terratest-terraform-fluent/testerror"
-	"github.com/gruntwork-io/terratest/modules/terraform"
-)
-
-// Apply runs terraform apply for the given Response and returns the error.
-func (resp Response) Plan() *testerror.Error {
-	_, err := terraform.PlanE(resp.t, resp.Options)
-	if err != nil {
-		return testerror.New(err.Error())
-	}
-	return nil
-}
-
-// // Apply runs terraform apply for the given Response and returns the error.
+// Plan runs terraform apply for the given Response and returns the error.
 // func (resp Response) Plan() *testerror.Error {
-// 	_, err := terraform.Sho(resp.t, resp.Options)
+// 	_, err := terraform.PlanE(resp.t, resp.Options)
 // 	if err != nil {
 // 		return testerror.New(err.Error())
 // 	}
 // 	return nil
-// }
+//}
