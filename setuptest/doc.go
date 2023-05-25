faa9fe6d7b6d11bfc9dcf8d706032ed78117059c
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-05-25 14:31:30 +0100

    doc: pkg updates

--- setuptest/doc.go
+++ setuptest/doc.go
@@ -1,5 +1,4 @@
 // Package setuptest is used to setup a test environment for testing Terraform code.
-//
 // It will copy the supplied directories to a temporary location before running
 // Terraform commands like init, plan, apply, destroy and output.
 package setuptest
